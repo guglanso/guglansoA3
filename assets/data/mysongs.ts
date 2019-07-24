ca769634c7e40c2053e1b03c583fecb618353bb4
guglanso <52787467+guglanso@users.noreply.github.com>
2019-07-24 18:50:07 -0400

    Add files via upload

---
+++ assets/data/mysongs.ts
@@ -0,0 +1,7 @@
+import {detail} from '../../app/detail' ;
+export const MYDETAILS : detail[]=[
+    {songname : 'Bekhayali' , artistname : 'Sachet tandon', genre : 'sad' , yearreleased : 2019 , pic : 'one.jpg' },
+    {songname : 'Kaise Hua' , artistname : 'Vishal Mishra', genre : 'pop' , yearreleased : 2019 , pic : 'two.jpg' },
+    {songname : 'Dheeme Dheeme' , artistname : 'Tony Kakkar', genre : 'rock' , yearreleased : 2019 , pic : 'three.jpg' },
+    {songname : 'Koka' , artistname : 'Jasbir Jassi', genre : 'jazz' , yearreleased : 2019 , pic : 'four.jpg' }
+]
